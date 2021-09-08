ba1828e594f2e65fa69454f02324fcb2e76566b0
manalkaff <manalkaff@gmail.com>
2021-09-08 13:11:46 +0800

    added __init__

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 setup(
     name='pyver',
     packages=find_packages(),
-    version='0.1.1',
+    version='0.1.2',
     description='Python Advanced HTTP Server',
     author='Abd Rahman Alkaff',
     license='MIT',
